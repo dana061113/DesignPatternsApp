9cd255e48b56bb04c64b912806f456ec639f0d38
dana061113 <danakobylandykyzy@gmail.com>
2026-03-01 19:00:17 +0500

    Implement CreditCardPayment strategy class

--- STRATEGY/CreditCardPayment.java
+++ STRATEGY/CreditCardPayment.java
@@ -1,1 +1,9 @@
+package strategy;
 
+public class CreditCardPayment implements PaymentStrategy {
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Төлем " + amount + " тг банк картасы арқылы орындалды.");
+    }
+}
